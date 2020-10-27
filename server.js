d6aa63b64a79ad7c0729337338674759965b7145
Jada Arnett <jada.arnett@gmail.com>
2020-10-26 21:56:20 -0400

    adding in process env to deploy to heroku

--- server.js
+++ server.js
@@ -15,14 +15,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
